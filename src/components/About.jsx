42c8d3de3879602d8be52d46daedfd68e9e6961e
Gabriel Dela Pena <delapena.gabriel12@gmail.com>
2023-05-14 17:18:27 +0200

    Fixed resizing bugs.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,20 +6,8 @@ const About = () => {
         <section name="about" className='max-w-[1400px] mx-auto py-28 px-4'>
             <div className='w-full flex flex-col mx-auto justify-center items-center lg:flex-row'>
                 <motion.div
-                    className='flex flex-col gap-2'
-                    /** initial condition */
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: true, amount: 0.5 }}
-                    transition={{ duration: 0.5 }}
-                    variants={{
-                        /** final conditions
-                         * x: -50 : div will be at -50 from the start
-                         * x: 0 : div will be at 0 from the start
-                         */
-                        hidden: { opacity: 0, x: -50 },
-                        visible: { opacity: 1, x: 0 },
-                    }}>
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}>
                     <p className='text-[#afafaf]'>WHO ARE WE</p>
                     <p className='font-extrabold text-5xl text-[#2f2f2f]'>About Us</p>
                     <img className="mt-4 p-6" src={SingleImage} alt="Single Image" />
@@ -27,19 +15,8 @@ const About = () => {
 
                 <motion.div
                     className="p-4 max-w-[500px] flex flex-col gap-6 text-[#757575] text-lg"
-                    /** initial condition */
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: true, amount: 0.5 }}
-                    transition={{ duration: 0.5 }}
-                    variants={{
-                        /** final conditions
-                         * x: -50 : div will be at -50 from the start
-                         * x: 0 : div will be at 0 from the start
-                         */
-                        hidden: { opacity: 0, x: 50 },
-                        visible: { opacity: 1, x: 0 },
-                    }}>
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}>
                     <p>
                         Welcome to our architecture portfolio!
                         We are a team of passionate architects committed to creating innovative and sustainable design solutions.
